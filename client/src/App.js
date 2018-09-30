d49ca32bcd5a886f231ac7375c785a04f2fa5a06
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2018-09-30 07:49:03 -0400

    Client setup add Reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import TopNavbar from "./components/TopNavbar";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<TopNavbar />
+				<h1>Hello</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
